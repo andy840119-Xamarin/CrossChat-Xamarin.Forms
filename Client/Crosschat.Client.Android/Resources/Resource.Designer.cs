a84905b58748024574de28ed340e22f968aa7c1a
andy840119 <andy840119@gmail.com>
2018-03-26 12:26:14 +0800

    update nuget

--- Client/Crosschat.Client.Android/Resources/Resource.Designer.cs
+++ Client/Crosschat.Client.Android/Resources/Resource.Designer.cs
@@ -31,12 +31,162 @@ namespace Crosschat.Client.Droid
 		public partial class Attribute
 		{
 			
+			// aapt resource value: 0x7f010007
+			public const int font = 2130771975;
+			
+			// aapt resource value: 0x7f010000
+			public const int fontProviderAuthority = 2130771968;
+			
+			// aapt resource value: 0x7f010003
+			public const int fontProviderCerts = 2130771971;
+			
+			// aapt resource value: 0x7f010004
+			public const int fontProviderFetchStrategy = 2130771972;
+			
+			// aapt resource value: 0x7f010005
+			public const int fontProviderFetchTimeout = 2130771973;
+			
+			// aapt resource value: 0x7f010001
+			public const int fontProviderPackage = 2130771969;
+			
+			// aapt resource value: 0x7f010002
+			public const int fontProviderQuery = 2130771970;
+			
+			// aapt resource value: 0x7f010006
+			public const int fontStyle = 2130771974;
+			
+			// aapt resource value: 0x7f010008
+			public const int fontWeight = 2130771976;
+			
 			static Attribute()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 			}
 			
 			private Attribute()
+			{
+			}
+		}
+		
+		public partial class Boolean
+		{
+			
+			// aapt resource value: 0x7f090000
+			public const int abc_action_bar_embed_tabs = 2131296256;
+			
+			static Boolean()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Boolean()
+			{
+			}
+		}
+		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f060003
+			public const int notification_action_color_filter = 2131099651;
+			
+			// aapt resource value: 0x7f060004
+			public const int notification_icon_bg_color = 2131099652;
+			
+			// aapt resource value: 0x7f060000
+			public const int notification_material_background_media_default_color = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int primary_text_default_material_dark = 2131099649;
+			
+			// aapt resource value: 0x7f060005
+			public const int ripple_material_light = 2131099653;
+			
+			// aapt resource value: 0x7f060002
+			public const int secondary_text_default_material_dark = 2131099650;
+			
+			// aapt resource value: 0x7f060006
+			public const int secondary_text_default_material_light = 2131099654;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
+		public partial class Dimension
+		{
+			
+			// aapt resource value: 0x7f0a0004
+			public const int compat_button_inset_horizontal_material = 2131361796;
+			
+			// aapt resource value: 0x7f0a0005
+			public const int compat_button_inset_vertical_material = 2131361797;
+			
+			// aapt resource value: 0x7f0a0006
+			public const int compat_button_padding_horizontal_material = 2131361798;
+			
+			// aapt resource value: 0x7f0a0007
+			public const int compat_button_padding_vertical_material = 2131361799;
+			
+			// aapt resource value: 0x7f0a0008
+			public const int compat_control_corner_material = 2131361800;
+			
+			// aapt resource value: 0x7f0a0009
+			public const int notification_action_icon_size = 2131361801;
+			
+			// aapt resource value: 0x7f0a000a
+			public const int notification_action_text_size = 2131361802;
+			
+			// aapt resource value: 0x7f0a000b
+			public const int notification_big_circle_margin = 2131361803;
+			
+			// aapt resource value: 0x7f0a0001
+			public const int notification_content_margin_start = 2131361793;
+			
+			// aapt resource value: 0x7f0a000c
+			public const int notification_large_icon_height = 2131361804;
+			
+			// aapt resource value: 0x7f0a000d
+			public const int notification_large_icon_width = 2131361805;
+			
+			// aapt resource value: 0x7f0a0002
+			public const int notification_main_column_padding_top = 2131361794;
+			
+			// aapt resource value: 0x7f0a0003
+			public const int notification_media_narrow_margin = 2131361795;
+			
+			// aapt resource value: 0x7f0a000e
+			public const int notification_right_icon_size = 2131361806;
+			
+			// aapt resource value: 0x7f0a0000
+			public const int notification_right_side_padding_top = 2131361792;
+			
+			// aapt resource value: 0x7f0a000f
+			public const int notification_small_icon_background_padding = 2131361807;
+			
+			// aapt resource value: 0x7f0a0010
+			public const int notification_small_icon_size_as_large = 2131361808;
+			
+			// aapt resource value: 0x7f0a0011
+			public const int notification_subtext_size = 2131361809;
+			
+			// aapt resource value: 0x7f0a0012
+			public const int notification_top_pad = 2131361810;
+			
+			// aapt resource value: 0x7f0a0013
+			public const int notification_top_pad_large_text = 2131361811;
+			
+			static Dimension()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Dimension()
 			{
 			}
 		}
@@ -56,6 +206,42 @@ namespace Crosschat.Client.Droid
 			// aapt resource value: 0x7f020003
 			public const int Icon = 2130837507;
 			
+			// aapt resource value: 0x7f020004
+			public const int notification_action_background = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int notification_bg = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int notification_bg_low = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int notification_bg_low_normal = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int notification_bg_low_pressed = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int notification_bg_normal = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int notification_bg_normal_pressed = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int notification_icon_background = 2130837515;
+			
+			// aapt resource value: 0x7f02000e
+			public const int notification_template_icon_bg = 2130837518;
+			
+			// aapt resource value: 0x7f02000f
+			public const int notification_template_icon_low_bg = 2130837519;
+			
+			// aapt resource value: 0x7f02000c
+			public const int notification_tile_bg = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int notify_panel_notification_icon_bg = 2130837517;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -69,20 +255,143 @@ namespace Crosschat.Client.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f040003
-			public const int image = 2130968579;
+			// aapt resource value: 0x7f04001d
+			public const int action0 = 2130968605;
 			
-			// aapt resource value: 0x7f040004
-			public const int message = 2130968580;
+			// aapt resource value: 0x7f04001a
+			public const int action_container = 2130968602;
 			
-			// aapt resource value: 0x7f040002
-			public const int nick = 2130968578;
+			// aapt resource value: 0x7f040021
+			public const int action_divider = 2130968609;
 			
-			// aapt resource value: 0x7f040001
-			public const int nicknameAndTimeWrapper = 2130968577;
+			// aapt resource value: 0x7f04001b
+			public const int action_image = 2130968603;
+			
+			// aapt resource value: 0x7f04001c
+			public const int action_text = 2130968604;
+			
+			// aapt resource value: 0x7f04002b
+			public const int actions = 2130968619;
+			
+			// aapt resource value: 0x7f040010
+			public const int async = 2130968592;
+			
+			// aapt resource value: 0x7f040011
+			public const int blocking = 2130968593;
+			
+			// aapt resource value: 0x7f04001e
+			public const int cancel_action = 2130968606;
+			
+			// aapt resource value: 0x7f040026
+			public const int chronometer = 2130968614;
+			
+			// aapt resource value: 0x7f04002d
+			public const int end_padder = 2130968621;
+			
+			// aapt resource value: 0x7f040012
+			public const int forever = 2130968594;
 			
 			// aapt resource value: 0x7f040000
-			public const int wrapper = 2130968576;
+			public const int ghost_view = 2130968576;
+			
+			// aapt resource value: 0x7f040028
+			public const int icon = 2130968616;
+			
+			// aapt resource value: 0x7f04002c
+			public const int icon_group = 2130968620;
+			
+			// aapt resource value: 0x7f040018
+			public const int image = 2130968600;
+			
+			// aapt resource value: 0x7f040027
+			public const int info = 2130968615;
+			
+			// aapt resource value: 0x7f040013
+			public const int italic = 2130968595;
+			
+			// aapt resource value: 0x7f04000a
+			public const int line1 = 2130968586;
+			
+			// aapt resource value: 0x7f04000b
+			public const int line3 = 2130968587;
+			
+			// aapt resource value: 0x7f040020
+			public const int media_actions = 2130968608;
+			
+			// aapt resource value: 0x7f040019
+			public const int message = 2130968601;
+			
+			// aapt resource value: 0x7f040017
+			public const int nick = 2130968599;
+			
+			// aapt resource value: 0x7f040016
+			public const int nicknameAndTimeWrapper = 2130968598;
+			
+			// aapt resource value: 0x7f040014
+			public const int normal = 2130968596;
+			
+			// aapt resource value: 0x7f04002a
+			public const int notification_background = 2130968618;
+			
+			// aapt resource value: 0x7f040023
+			public const int notification_main_column = 2130968611;
+			
+			// aapt resource value: 0x7f040022
+			public const int notification_main_column_container = 2130968610;
+			
+			// aapt resource value: 0x7f040001
+			public const int parent_matrix = 2130968577;
+			
+			// aapt resource value: 0x7f040029
+			public const int right_icon = 2130968617;
+			
+			// aapt resource value: 0x7f040024
+			public const int right_side = 2130968612;
+			
+			// aapt resource value: 0x7f040002
+			public const int save_image_matrix = 2130968578;
+			
+			// aapt resource value: 0x7f040003
+			public const int save_non_transition_alpha = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int save_scale_type = 2130968580;
+			
+			// aapt resource value: 0x7f04001f
+			public const int status_bar_latest_event_content = 2130968607;
+			
+			// aapt resource value: 0x7f04000c
+			public const int tag_transition_group = 2130968588;
+			
+			// aapt resource value: 0x7f04000d
+			public const int text = 2130968589;
+			
+			// aapt resource value: 0x7f04000e
+			public const int text2 = 2130968590;
+			
+			// aapt resource value: 0x7f040025
+			public const int time = 2130968613;
+			
+			// aapt resource value: 0x7f04000f
+			public const int title = 2130968591;
+			
+			// aapt resource value: 0x7f040005
+			public const int transition_current_scene = 2130968581;
+			
+			// aapt resource value: 0x7f040006
+			public const int transition_layout_save = 2130968582;
+			
+			// aapt resource value: 0x7f040007
+			public const int transition_position = 2130968583;
+			
+			// aapt resource value: 0x7f040008
+			public const int transition_scene_layoutid_cache = 2130968584;
+			
+			// aapt resource value: 0x7f040009
+			public const int transition_transform = 2130968585;
+			
+			// aapt resource value: 0x7f040015
+			public const int wrapper = 2130968597;
 			
 			static Id()
 			{
@@ -90,6 +399,25 @@ namespace Crosschat.Client.Droid
 			}
 			
 			private Id()
+			{
+			}
+		}
+		
+		public partial class Integer
+		{
+			
+			// aapt resource value: 0x7f070000
+			public const int cancel_button_image_alpha = 2131165184;
+			
+			// aapt resource value: 0x7f070001
+			public const int status_bar_notification_info_maxnum = 2131165185;
+			
+			static Integer()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Integer()
 			{
 			}
 		}
@@ -109,12 +437,187 @@ namespace Crosschat.Client.Droid
 			// aapt resource value: 0x7f030003
 			public const int message_item_owner = 2130903043;
 			
+			// aapt resource value: 0x7f030004
+			public const int notification_action = 2130903044;
+			
+			// aapt resource value: 0x7f030005
+			public const int notification_action_tombstone = 2130903045;
+			
+			// aapt resource value: 0x7f030006
+			public const int notification_media_action = 2130903046;
+			
+			// aapt resource value: 0x7f030007
+			public const int notification_media_cancel_action = 2130903047;
+			
+			// aapt resource value: 0x7f030008
+			public const int notification_template_big_media = 2130903048;
+			
+			// aapt resource value: 0x7f030009
+			public const int notification_template_big_media_custom = 2130903049;
+			
+			// aapt resource value: 0x7f03000a
+			public const int notification_template_big_media_narrow = 2130903050;
+			
+			// aapt resource value: 0x7f03000b
+			public const int notification_template_big_media_narrow_custom = 2130903051;
+			
+			// aapt resource value: 0x7f03000c
+			public const int notification_template_custom_big = 2130903052;
+			
+			// aapt resource value: 0x7f03000d
+			public const int notification_template_icon_group = 2130903053;
+			
+			// aapt resource value: 0x7f03000e
+			public const int notification_template_lines_media = 2130903054;
+			
+			// aapt resource value: 0x7f03000f
+			public const int notification_template_media = 2130903055;
+			
+			// aapt resource value: 0x7f030010
+			public const int notification_template_media_custom = 2130903056;
+			
+			// aapt resource value: 0x7f030011
+			public const int notification_template_part_chronometer = 2130903057;
+			
+			// aapt resource value: 0x7f030012
+			public const int notification_template_part_time = 2130903058;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 			}
 			
 			private Layout()
+			{
+			}
+		}
+		
+		public partial class String
+		{
+			
+			// aapt resource value: 0x7f080000
+			public const int status_bar_notification_info_overflow = 2131230720;
+			
+			static String()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050005
+			public const int TextAppearance_Compat_Notification = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int TextAppearance_Compat_Notification_Info = 2131034118;
+			
+			// aapt resource value: 0x7f050000
+			public const int TextAppearance_Compat_Notification_Info_Media = 2131034112;
+			
+			// aapt resource value: 0x7f05000b
+			public const int TextAppearance_Compat_Notification_Line2 = 2131034123;
+			
+			// aapt resource value: 0x7f050004
+			public const int TextAppearance_Compat_Notification_Line2_Media = 2131034116;
+			
+			// aapt resource value: 0x7f050001
+			public const int TextAppearance_Compat_Notification_Media = 2131034113;
+			
+			// aapt resource value: 0x7f050007
+			public const int TextAppearance_Compat_Notification_Time = 2131034119;
+			
+			// aapt resource value: 0x7f050002
+			public const int TextAppearance_Compat_Notification_Time_Media = 2131034114;
+			
+			// aapt resource value: 0x7f050008
+			public const int TextAppearance_Compat_Notification_Title = 2131034120;
+			
+			// aapt resource value: 0x7f050003
+			public const int TextAppearance_Compat_Notification_Title_Media = 2131034115;
+			
+			// aapt resource value: 0x7f050009
+			public const int Widget_Compat_NotificationActionContainer = 2131034121;
+			
+			// aapt resource value: 0x7f05000a
+			public const int Widget_Compat_NotificationActionText = 2131034122;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
+			{
+			}
+		}
+		
+		public partial class Styleable
+		{
+			
+			public static int[] FontFamily = new int[] {
+					2130771968,
+					2130771969,
+					2130771970,
+					2130771971,
+					2130771972,
+					2130771973};
+			
+			// aapt resource value: 0
+			public const int FontFamily_fontProviderAuthority = 0;
+			
+			// aapt resource value: 3
+			public const int FontFamily_fontProviderCerts = 3;
+			
+			// aapt resource value: 4
+			public const int FontFamily_fontProviderFetchStrategy = 4;
+			
+			// aapt resource value: 5
+			public const int FontFamily_fontProviderFetchTimeout = 5;
+			
+			// aapt resource value: 1
+			public const int FontFamily_fontProviderPackage = 1;
+			
+			// aapt resource value: 2
+			public const int FontFamily_fontProviderQuery = 2;
+			
+			public static int[] FontFamilyFont = new int[] {
+					16844082,
+					16844083,
+					16844095,
+					2130771974,
+					2130771975,
+					2130771976};
+			
+			// aapt resource value: 0
+			public const int FontFamilyFont_android_font = 0;
+			
+			// aapt resource value: 2
+			public const int FontFamilyFont_android_fontStyle = 2;
+			
+			// aapt resource value: 1
+			public const int FontFamilyFont_android_fontWeight = 1;
+			
+			// aapt resource value: 4
+			public const int FontFamilyFont_font = 4;
+			
+			// aapt resource value: 3
+			public const int FontFamilyFont_fontStyle = 3;
+			
+			// aapt resource value: 5
+			public const int FontFamilyFont_fontWeight = 5;
+			
+			static Styleable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Styleable()
 			{
 			}
 		}
